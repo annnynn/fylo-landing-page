c1e90be654a849aef17788cfc1d9958d45a68f55
annyn <ennibondarenko@gmail.com>
2023-02-21 16:55:49 +0400

    commit

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 function validateEmail() {
     var form = document.getElementById("form");
-    var email = document.getElementById("email").value;
+    var emailInput = document.getElementById("email").value;
     var error = document.getElementById("error");
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
-    if (email.match(pattern)) {
-        form.classList.add("valid");
-        form.classList.remove("invalid");
-        error.innerHTML = "Email is correct";
-        error.style.color = "#00ff00";
-    } else {
-        form.classList.remove("valid");
-        form.classList.add("invalid");
-        error.innerHTML = "check your email";
-        error.style.color = "#ff0000";
-    }
-
-     if(email == ""){
-        form.classList.remove("valid");
-        form.classList.remove("invalid");
-        error.innerHTML = "";
-        error.style.color = "#00ff00";
-     }
+    
+   
 }
